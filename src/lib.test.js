7b4476feb150b19d6c25f569ce9f085a2cb6e078
Bo Yang <bo2.yang@symbio.com>
2019-01-22 18:38:39 +0100

    make test pass

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,5 +1,5 @@
 const lib = require('./lib');
 
 test('helloWorld() returns some special text', () => {
-  expect(lib.helloWorld()).toBe('Hello World');
+  expect(lib.helloWorld()).toBe('Hello World YangBo');
 });
